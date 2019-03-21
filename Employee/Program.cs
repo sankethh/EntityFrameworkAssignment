58275abeb82bcfd5b47988a0207d409a965cfd83
sankethh <48273862+sankethh@users.noreply.github.com>
2019-03-21 15:21:25 +0530

    Add files via upload

---
+++ Employee/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using EFemp;
+namespace EFemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Flag = 1;
+            int choice;
+            while (Flag == 1)
+            {
+                Console.WriteLine("Choose an option from the below: - \n");
+                Console.WriteLine("1) Insert from CSV into DATABASE \n 2) Get all employee Details according to your location \n");
+                Console.WriteLine("3) Get all employee details greater than the date given by user \n 4) exit \n");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        string userchoice = Program.Csvinsert();
+                        if (userchoice == "n")
+                        {
+                            Flag = 0;
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the location to filter \n");
+                        string location = Console.ReadLine();
+                        Program.Locationmapping(location);
+                        break;
+                }
+            }
+        }
+        public static void Locationmapping(string location)
+        {
+            EmployeeEntities _repo = new EmployeeEntities();
+            EMP emp = new EMP();
+
+            List<EMP> query = empdb.EID.Where(s => s.LOCATION == location).ToList());
+            foreach (var employeeid in query)
+            {
+                string formateddateofbirth = String.Format("{0:MM/dd/yyyy}", emp.DOB);
+                string formateddateofjoining = String.Format("{0:MM/dd/yyyy}", emp.DOJ);
+                Console.WriteLine($"EID : {employeeid.EID} NAME : {employeeid.ENAME} DOB : {emp.DOB} Location : {emp.LOCATION} DOJ : {formateddateofjoining} \n");
+                Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+            }
+        }
+        public static string Csvinsert()
+        {
+            List<MyMappedCSVFile> values = File.ReadAllLines(@"C:\Users\Sanket\Desktop\Neudesic\CSVRecord")
+                                           .Skip(1)
+                                           .Select(v => MyMappedCSVFile.FromCsv(v))
+                                           .ToList();
+            foreach (var rowl in values)
+            {
+                EmployeeEntities _repo = new EmployeeEntities();
+                EMP emp = new EMP();
+                Console.WriteLine(rowl);
+                emp.EID = rowl.EID;
+                emp.ENAME = rowl.Name;
+                emp.DOB = rowl.DOB;
+                emp.LOCATION = rowl.location;
+                emp.DOJ = rowl.DOJ;
+                try
+                {
+                    _repo.EMPs.Add(emp);
+                    _repo.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString() + "Warning");
+                }
+            }
+            Console.WriteLine("\n Want to enter another option ? y/n");
+            string choice2 = Console.ReadLine();
+            return choice2;
+        }
+
+        public class MyMappedCSVFile
+        {
+            public string Name { get; set; }
+            public System.DateTime DOB { get; set; }
+            public int EID { get; set; }
+            public string location { get; set; }
+            public System.DateTime DOJ { get; set; }
+            public override string ToString()
+            {
+                return "Person: " + EID + " " + Name + " " + DOB + " " + location + " " + DOJ;
+            }
+            public static MyMappedCSVFile FromCsv(string csvLine)
+            {
+                string[] values = csvLine.Split(',');
+                MyMappedCSVFile obj = new MyMappedCSVFile();
+                obj.EID = Convert.ToInt32(values[0]);
+                obj.Name = Convert.ToString(values[2]);
+                obj.DOB = Convert.ToDateTime(values[10]);
+                obj.location = Convert.ToString(values[34]);
+                obj.DOJ = Convert.ToDateTime(values[14]);
+                return obj;
+            }
+        }
+    }
+}
